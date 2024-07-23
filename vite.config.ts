97f182034dc761d2a84b0bdffb23fc8dd588394b
yjy <yuanjinyun@defencedriver.com>
2024-07-23 17:24:26 +0800

    打包

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { setupBuild } from "./build/index.ts"
+import { setupBuild } from "./build/index"
 
 
 export default defineConfig(({ mode }) => ({
