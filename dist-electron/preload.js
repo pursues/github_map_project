5bb73f8544aa88559616d01a720e6954d0227766
Mr930212 <1696348253@qq.com>
2024-07-23 09:50:19 +0800

    取消桌面应用

--- dist-electron/preload.js
+++
@@ -1,1 +0,0 @@
-"use strict";var o=require("os");function a(t){return t&&typeof t=="object"&&"default"in t?t:{default:t}}var e=a(o);console.log("platform",e.default.platform());
--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
- 
-import electron from "vite-plugin-electron"
-import electronRenderer from "vite-plugin-electron-renderer" 
-import polyfillExports from "vite-plugin-electron-renderer" 
- 
+
+
 export default defineConfig(({ mode }) => ({
-  base: mode == 'development' ? '' : './',
+  base: './',
   plugins: [
-    vue(),
-    electron([{
-      entry: "electron/main.ts", // 主进程文件
-    },
-    {
-      entry: 'preload/preload.ts'
-    }
-  ]),
-    electronRenderer(),
-    polyfillExports(),
-  ],
-  build: {
-    emptyOutDir: false, // 默认情况下，若 outDir 在 root 目录下，则 Vite 会在构建时清空该目录
-    outDir: "dist-electron"
-  },
+    vue(),]
 }))
