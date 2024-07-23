39d4a700b6c02436dcc83e24c48ad08de18e5b64
yjy <yuanjinyun@defencedriver.com>
2024-07-23 17:03:20 +0800

    build

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,13 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { setupBuild } from "./build/index.ts"
 
 
 export default defineConfig(({ mode }) => ({
   base: './',
   publicPath:'/github_map_project/',
   baseUrl:'/github_map_project/',
-  build:{
-    outDir:'dist'
-  },
+  build:setupBuild(),
   plugins: [
     vue(),]
 }))
