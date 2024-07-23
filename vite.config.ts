3c3a39f29cd4b8d553d52b94023eb967c5ef811c
yjy <yuanjinyun@defencedriver.com>
2024-07-23 16:38:36 +0800

    打包

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig(({ mode }) => ({
   base: '/github_map_project/',
+  publicPath:'/github_map_project/',
+  build:{
+    outDir:'dist'
+  },
   plugins: [
     vue(),]
 }))
